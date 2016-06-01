b605a43204f34ee8426e9eb6bcc9c5990947795b
Octowl <alsaff1987@gmail.com>
2016-06-01 16:41:53 -0400

    Add validation for type

--- models/node.model.js
+++ models/node.model.js
@@ -5,9 +5,18 @@
 var db = require('./db');
 var Sequelize = require('sequelize');
 
+var allowedTypes = ['bias', 'input', 'output', 'hidden'];
+
 var Node = db.define('node', {
     type: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            isIn: {
+                args: allowedTypes,
+                msg: "Must be one of " + allowedTypes.join(', ')
+            }
+        }
     }
 });
 
--- test/node.model.spec.js
+++ test/node.model.spec.js
@@ -32,6 +32,35 @@ describe("Node", function () {
             expect(node.type).to.equal('input');
         });
 
+        it("is required", function () {
+            var node = Node.build();
+            return node.validate()
+                .then(function (result) {
+                    expect(result).to.be.an('object');
+                    expect(result.message).to.equal('notNull Violation: type cannot be null');
+                });
+        });
+
+        it("can only be one of the approved types", function () {
+            var node;
+            var allowedTypes = ['bias', 'input', 'output', 'hidden'];
+            allowedTypes.forEach(function (type) {
+                node = Node.build({
+                    type: type
+                });
+                expect(node.type).to.equal(type);
+            });
+
+            node = Node.build({
+                type: 'definitelynotarealtype'
+            });
+            return node.validate()
+                .then(function (result) {
+                    expect(result).to.be.an('object');
+                    expect(result.message).to.equal('Validation error: Must be one of ' + allowedTypes.join(', '));
+                });
+        });
+
     });
 
 });
